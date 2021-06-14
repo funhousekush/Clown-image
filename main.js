daa769595c5299e6163c5ae0a4a59a5ea03e842e
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-06-14 20:57:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){}
+
+function setup()
+{
+    var canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){}
+
+function take_snapshot()
+{
+    save("MyClownimage.jpg");
+}
